ff8094a31219f70dd1763386e2d396faab2e29ca
Denis Mamatin <mamatin-denis@yandex.ru>
2023-10-01 00:50:09 +0300

    fix bug coordinates reseted

--- src/main/java/ru/mipt/bit/platformer/action/Move.java
+++ src/main/java/ru/mipt/bit/platformer/action/Move.java
@@ -3,11 +3,19 @@ package ru.mipt.bit.platformer.action;
 import com.badlogic.gdx.math.GridPoint2;
 import ru.mipt.bit.platformer.gameEntities.GameEntity;
 
-class Move implements Action{
+public enum Move implements Action{
+    UP(new GridPoint2(0, 1), 90f),
+    DOWN(new GridPoint2(0, -1), -90f),
+    LEFT(new GridPoint2(-1, 0), 180f),
+    RIGHT(new GridPoint2(1, 0), 0f),
+    ;
 
-    Move(Direction direction) {
-        this.coordinates = direction.getCoordinates();
-        this.rotation = direction.getRotation();
+    private boolean coordinatesReseted;
+
+    Move(GridPoint2 coordinates, float rotation) {
+        this.coordinates = coordinates;
+        this.rotation = rotation;
+        this.coordinatesReseted = false;
     }
 
     public GridPoint2 getCoordinates() {
@@ -15,11 +23,17 @@ class Move implements Action{
     }
 
     public void apply(GameEntity gameEntity) {
-        gameEntity.moveToDirection(coordinates, rotation);
+        if (coordinatesReseted){
+            gameEntity.moveToDirection(new GridPoint2(0, 0), rotation);
+            coordinatesReseted = false;
+        }
+        else {
+            gameEntity.moveToDirection(coordinates, rotation);
+        }
     }
 
     public void resetCoordinates(){
-        coordinates.set(new GridPoint2(0, 0));
+        coordinatesReseted = true;
     }
 
     private final float rotation;
